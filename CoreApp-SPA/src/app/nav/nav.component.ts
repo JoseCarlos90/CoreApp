5f50496e24a544e04c5d389fce5576f9d5803f41
Jossu <jossu_sw000@hotmail.com>
2018-12-26 23:40:42 +0100

    End of section 6

--- CoreApp-SPA/src/app/nav/nav.component.ts
+++ CoreApp-SPA/src/app/nav/nav.component.ts
@@ -1,16 +1,17 @@
+import { AlertifyService } from './../_services/alertify.service';
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
-import { tokenKey } from '@angular/core/src/view';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css']
 })
+
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor(private authService: AuthService) { }
+  constructor(public authService: AuthService, private alertify: AlertifyService) { }
 
   ngOnInit() {
   }
@@ -18,19 +19,18 @@ export class NavComponent implements OnInit {
   login() {
     this.authService.login(this.model)
     .subscribe(next => {
-      console.log('Legeado correctamente');
+      this.alertify.success('Login realizado con exito!');
     }, error => {
-      console.log(error);
+      this.alertify.error(error);
     });
   }
 
   loggedIn() {
-    const token = localStorage.getItem('token');
-    return !!token;
+    return this.authService.loggedIn();
   }
 
   logout() {
     localStorage.removeItem('token');
-    console.log('LogOut!!');
+    this.alertify.message('LogOut realizado con exito!');
   }
 }
